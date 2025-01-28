e57ecb0ee61d2597b6f63e504535d0e0fdb1244d
alamin <alameen@ibos.io>
2025-01-28 10:10:43 +0600

    udate

--- src/config/firestore.js
+++ src/config/firestore.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCpY-E-FN6h391W2-jyc91UJLAVOoCYFm4",
-  authDomain: "employee-database-99db2.firebaseapp.com",
-  projectId: "employee-database-99db2",
-  storageBucket: "employee-database-99db2.firebasestorage.app",
-  messagingSenderId: "470167916629",
-  appId: "1:470167916629:web:c0ef4c5996de65090384dc",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
